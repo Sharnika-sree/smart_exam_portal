fe175a5f0124b70c80f0cc1de6d1f051357fd1d1
sharnika <sharnikasreesp.24cse@kongu.edu>
2026-01-14 21:46:20 +0530

    Initial commit

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../App.css";
+
+const LoginPage = () => {
+  return (
+    <div className="login-container">
+      <div className="left-side">
+        <h1>WELCOME TO EXAM PORTAL</h1>
+        <p>this is safe and secure</p>
+      </div>
+
+      <div className="right-side">
+        <h2>Sign Up Today</h2>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
