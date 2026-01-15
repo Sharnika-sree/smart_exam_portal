02679f87a3278112608195e2fb33b9c94588517d
sharnika <sharnikasreesp.24cse@kongu.edu>
2026-01-15 15:35:01 +0530

    first commit

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import "../App.css";
+import examImage from "../assets/exam-illustration.png";
+
 
 const LoginPage = () => {
   return (
     <div className="login-container">
       <div className="left-side">
+        
         <h1>WELCOME TO EXAM PORTAL</h1>
         <p>this is safe and secure</p>
       </div>
 
       <div className="right-side">
-        <h2>Sign Up Today</h2>
+        <div className="login-box">
+        <h2>Login  Today</h2>
+        <form>
+          <div className="form-group">
+            <label>Email ID </label>
+            <input type="email" placeholder="Enter email id" />
+                          <label>Password</label>
+            <input type="password" placeholder="enter password"></input>   
+                   </div>
+                   <button type="submit">Login</button>
+                   </form>
+                   <p className="help-text">having trouble logging in? contact the administrator.</p>
       </div>
+    </div>
     </div>
   );
 };
